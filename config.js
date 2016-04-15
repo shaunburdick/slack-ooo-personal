ff405a6e8a5211717f6516dffb66d40e49b8e2ca
Jeremiah Kendall <monitor@blueskybroadcast.com>
2016-04-15 16:10:25 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
   app: {
-    message: '', // The message you want to send to users
+    message: 'I am on vacation until Monday, April 25th', // The message you want to send to users
     timebox: {
-      start: 0, // start timestamp, or parsable date (see http://dygraphs.com/date-formats.html)
-      end: 0, // end timestmap, or parsable date (see http://dygraphs.com/date-formats.html)
+      start: 2016/04/15, // start timestamp, or parsable date (see http://dygraphs.com/date-formats.html)
+      end: 2016/04/24, // end timestmap, or parsable date (see http://dygraphs.com/date-formats.html)
     },
     reminder: 480, // minutes, Do not respond to the user for another 8 hours
     respond: {
@@ -12,7 +12,7 @@ const config = {
     },
   },
   slack: {
-    token: 'xoxb-Your-Token', // get it from https://api.slack.com/web
+    token: 'xoxp-4897644109-4876068626-35211612884-ce16bd1e67', // get it from https://api.slack.com/web
     autoReconnect: true,
     autoMark: false, // Mark messages as read
   },
